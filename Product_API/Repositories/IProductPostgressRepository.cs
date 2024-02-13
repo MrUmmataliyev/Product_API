9ee458a752ceaf117784aeaddc694d3bb4aae669
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-02-13 15:48:14 +0500

    ATOM

--- Product_API/Repositories/IProductPostgressRepository.cs
+++ Product_API/Repositories/IProductPostgressRepository.cs
@@ -12,40 +12,40 @@ namespace Product_API.Repositories
 
     public Product Add(Product product)
         {
-            //using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
-            //{
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
 
-            //    connection.Open();
-            //    string query = @$"insert into products(Name,Description,PhotoPath) values ('{product.Name}','{product.Description}','{product.PhotoPath}');";
-            //        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
-            //        var reader = cmd.ExecuteNonQuery();
+                connection.Open();
+                string query = @$"insert into products(Name,Description,PhotoPath) values ('{product.Name}','{product.Description}','{product.PhotoPath}');";
+                    using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+                    var reader = cmd.ExecuteNonQuery();
 
-            //}
+            }
             return product;
         }
 
         public List<Product> GetAll()
         {
             List<Product> products = new List<Product>();
-            //using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
-            //{
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
 
-            //    connection.Open();
-            //    string query = @$"select * from products ";
-                 Product product = new Product();
-            //        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
-            //        var reader = cmd.ExecuteReader();
-            //        while (reader.Read())
-            //        {
-            //            product.Name = reader.GetString(0);
-            //            product.Description = reader.GetString(1);
-            //            product.PhotoPath = reader.GetString(2);
-            //            products.Add(product);
-            //        }
+                connection.Open();
+                string query = @$"select * from products ";
+                    Product product = new Product();
+                    using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+                    var read = cmd.ExecuteReader();
+                    while (read.Read())
+                    {
+                        product.Name = read.GetString(0);
+                        product.Description = read.GetString(1);
+                        product.PhotoPath = read.GetString(2);
+                        products.Add(product);
+                    }
 
               
-               return products;
-            //}
+            }
+            return products;
         }
     }
 }
